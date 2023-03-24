4f2a807c8b75dd22bd7bb970064e25b37736bdca
milovangudelj <milovan.gudelj@gmail.com>
2023-03-24 11:40:02 +0100

    Changed structure a little bit

--- components/NavbarCredits/NavbarCredits.tsx
+++ components/NavbarCredits/NavbarCredits.tsx
@@ -5,14 +5,15 @@ import NextLink from "next/link";
 
 export const NavbarCredits = () => {
 	return (
-		<span className="text-xs inline-flex items-center gap-1 font-medium opacity-60 hover:opacity-100 transition">
-			by{" "}
-			<NextLink href={"https://github.com/milovangudelj"} target="_blank">
-				@milovangudelj
-			</NextLink>{" "}
+		<NextLink
+			href={"https://github.com/milovangudelj"}
+			target="_blank"
+			className="text-xs inline-flex items-center gap-2 font-medium opacity-60 hover:opacity-100 transition"
+		>
+			by @milovangudelj{" "}
 			<span>
 				<GithubLogo aria-hidden={true} className="w-4 h-4" weight="bold" />
 			</span>
-		</span>
+		</NextLink>
 	);
 };
